e6931c6dfbf8d8fb57b431f5c4e146bf05d5b3b0
Diego De la Rocha <hi@diegodelarocha.com>
2020-05-07 22:08:49 -0700

    Migrating Assets to Package Manager, still got a bunch of errors with DitzelGames.FastIK Assemblies, but should be fixable soon?

---
+++ Packages/com.diegodelarocha.acorn/setup assets [not for production use]/scripts/Editor/avatarCreator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+ 
+namespace Acorn.Editor
+{
+    public class avatarCreator
+    /// <summary>
+    /// Creates Avatar and Avatar Masks of selected objects/rigs.
+    /// </summary>
+    {
+        [MenuItem("Tools/AvatarTools/CreateAvatarMask")]
+        private static void CreateAvatarMask()
+        {
+            GameObject activeGameObject = Selection.activeGameObject;
+ 
+            if (activeGameObject != null)
+            {
+                AvatarMask avatarMask = new AvatarMask();
+ 
+                avatarMask.AddTransformPath(activeGameObject.transform);
+ 
+                var path = string.Format("Assets/{0}.mask", activeGameObject.name.Replace(':', '_'));
+                AssetDatabase.CreateAsset(avatarMask, path);
+            }
+        }
+ 
+        [MenuItem("Tools/AvatarTools/CreateAvatar")]
+        private static void CreateAvatar()
+        {
+            GameObject activeGameObject = Selection.activeGameObject;
+ 
+            if (activeGameObject != null)
+            {
+                Avatar avatar = AvatarBuilder.BuildGenericAvatar(activeGameObject, "");
+                avatar.name = activeGameObject.name;
+                Debug.Log(avatar.isHuman ? "is human" : "is generic");
+ 
+                var path = string.Format("Assets/{0}.ht", avatar.name.Replace(':', '_'));
+                AssetDatabase.CreateAsset(avatar, path);
+            }
+        }
+    }
+}
---
+++ Packages/com.diegodelarocha.acorn/setup assets [not for production use]/scripts/Runtime/FK_IK_switch.cs
@@ -0,0 +1,295 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EasyButtons;
+using DitzelGames;
+using UnityEngine;
+
+namespace Acorn
+{
+    
+    public class FK_IK_switch : MonoBehaviour
+    {
+    // IK-FK Switch Variables
+
+    // IK Script Component - FastIKLook
+        [HideInInspector] public DitzelGames.FastIK.FastIKLook head_fastIKLook;
+    // IK Script Component - FastIKFabric
+        [HideInInspector] public DitzelGames.FastIK.FastIKFabric leftHand_fastIKFabric;
+        [HideInInspector] public DitzelGames.FastIK.FastIKFabric rightHand_fastIKFabric;
+        [HideInInspector] public DitzelGames.FastIK.FastIKFabric leftFoot_fastIKFabric;
+        [HideInInspector] public DitzelGames.FastIK.FastIKFabric rightFoot_fastIKFabric;
+
+        
+    // Handle Renderers
+        [HideInInspector] public MeshRenderer head_handleRenderer;
+        [HideInInspector] public MeshRenderer[] leftHand_handleRenderer;
+        [HideInInspector] public MeshRenderer[] rightHand_handleRenderer;
+        [HideInInspector] public MeshRenderer[] leftFoot_handleRenderer;
+        [HideInInspector] public MeshRenderer[] rightFoot_handleRenderer;
+        [HideInInspector] public MeshRenderer[] root_handleRenderer;
+
+    // Visibility Variables
+        [Header("Handles Visibility (Play Mode)")]
+        public bool handlesVis;
+        
+        private bool head_Vis;
+        private bool leftHand_Vis;
+        private bool rightHand_Vis;
+        private bool leftFoot_Vis;
+        private bool rightFoot_Vis;
+        private bool root_Vis;
+
+    // Match FK-IK Transform Variables
+
+        [HideInInspector] public Transform leftHand;
+        [HideInInspector] public Transform leftHand_IKHandle;
+        [HideInInspector] public Transform rightHand;
+        [HideInInspector] public Transform rightHand_IKHandle;
+        [HideInInspector] public Transform leftFoot;
+        [HideInInspector] public Transform leftFoot_IKHandle;
+        [HideInInspector] public Transform rightFoot;
+        [HideInInspector] public Transform rightFoot_IKHandle;
+
+    // FK-IK Switch
+
+    // All FK
+
+        [Button("All FK", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+        void all_FK(){
+            head_FK();
+            leftHand_FK();
+            rightHand_FK();
+            leftFoot_FK();
+            rightFoot_FK();
+
+            Debug.Log("Switch All to FK");
+        }
+
+    // All IK
+    
+        [Button("All IK", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void all_IK(){
+            head_IK();
+            leftHand_IK();
+            rightHand_IK();
+            leftFoot_IK();
+            rightFoot_IK();
+
+            Debug.Log("Switch All to IK");
+        }
+
+    // Head
+        [Button("Head FK", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+        void head_FK(){
+            head_fastIKLook.enabled = false;
+            head_handleRenderer.enabled = false; 
+
+            Debug.Log("Switch Head to FK");      
+        }
+
+        [Button("Head IK", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void head_IK(){
+            head_fastIKLook.enabled = true;
+            head_handleRenderer.enabled = true;
+
+            Debug.Log("Switch Head to IK");
+        }
+
+    // Left Hand
+
+        [Button("Left Hand FK", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+        void leftHand_FK(){
+            leftHand_fastIKFabric.enabled = false;
+            
+            for(int i = 0; i < leftHand_handleRenderer.Length; i++){
+                leftHand_handleRenderer[i].enabled = false;
+            }
+
+            Debug.Log("Switch Left Hand to FK");
+        }
+
+        [Button("Left Hand IK", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void leftHand_IK(){
+            leftHand_fastIKFabric.enabled = true;
+            
+            for(int i = 0; i < leftHand_handleRenderer.Length; i++){
+                leftHand_handleRenderer[i].enabled = true;
+            }
+            
+            // Match FK-IK Transforms
+            leftHand_IKHandle.position = leftHand.position;
+            leftHand_IKHandle.rotation = leftHand.rotation;
+
+            Debug.Log("Switch Left Hand to IK, and Match IK to FK Position + Rotation");
+
+        }
+
+    // Right Hand
+
+        [Button("Right Hand FK", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+        void rightHand_FK(){
+            rightHand_fastIKFabric.enabled = false;
+            
+            for(int i = 0; i < rightHand_handleRenderer.Length; i++){
+                rightHand_handleRenderer[i].enabled = false;
+            }
+
+            Debug.Log("Switch Right Hand to FK");
+        }
+
+        [Button("Right Hand IK", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void rightHand_IK(){
+            rightHand_fastIKFabric.enabled = true;
+            
+            for(int i = 0; i < rightHand_handleRenderer.Length; i++){
+                rightHand_handleRenderer[i].enabled = true;
+            }
+            
+            // Match FK-IK Transforms
+            rightHand_IKHandle.position = rightHand.position;
+            rightHand_IKHandle.rotation = rightHand.rotation;
+
+            Debug.Log("Switch Right Hand to IK, and Match IK to FK Position + Rotation");
+        }
+
+
+    // Left Foot
+
+        [Button("Left Foot FK", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+        void leftFoot_FK(){
+            leftFoot_fastIKFabric.enabled = false;
+            
+            for(int i = 0; i < leftFoot_handleRenderer.Length; i++){
+                leftFoot_handleRenderer[i].enabled = false;
+            }
+
+            Debug.Log("Switch Left Foot to FK");
+        }
+
+        [Button("Left Foot IK", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void leftFoot_IK(){
+            leftFoot_fastIKFabric.enabled = true;
+            
+            for(int i = 0; i < leftFoot_handleRenderer.Length; i++){
+                leftFoot_handleRenderer[i].enabled = true;
+            }
+            
+            // Match FK-IK Transforms
+            leftFoot_IKHandle.position = leftFoot.position;
+            leftFoot_IKHandle.rotation = leftFoot.rotation;
+
+            Debug.Log("Switch Left Foot to IK, and Match IK to FK Position + Rotation");
+        }
+
+
+    // Right Foot
+
+        [Button("Right Foot FK", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+        void rightFoot_FK(){
+            rightFoot_fastIKFabric.enabled = false;
+            
+            for(int i = 0; i < rightFoot_handleRenderer.Length; i++){
+                rightFoot_handleRenderer[i].enabled = false;
+            }
+
+            Debug.Log("Switch Right Foot to FK");
+        }
+
+        [Button("Right Foot IK", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void rightFoot_IK(){
+            rightFoot_fastIKFabric.enabled = true;
+            
+            for(int i = 0; i < rightFoot_handleRenderer.Length; i++){
+                rightFoot_handleRenderer[i].enabled = true;
+            }
+            
+            // Match FK-IK Transforms
+            rightFoot_IKHandle.position = rightFoot.position;
+            rightFoot_IKHandle.rotation = rightFoot.rotation;
+
+            Debug.Log("Switch Right Foot to IK, and Match IK to FK Position + Rotation");
+        }
+
+    // Visibility Functions
+        void LateUpdate(){
+            VisAll();  
+        }
+        void VisAll(){
+            if(handlesVis == false){
+                VisOff();      
+            }
+            else{
+                VisOn();      
+            }
+        }
+        void Vis(){
+
+            head_handleRenderer.enabled = head_Vis;
+
+            for(int i = 0; i < leftHand_handleRenderer.Length; i++){
+            leftHand_handleRenderer[i].enabled = leftHand_Vis;
+            } 
+            for(int i = 0; i < rightHand_handleRenderer.Length; i++){
+            rightHand_handleRenderer[i].enabled = rightHand_Vis;
+            } 
+            for(int i = 0; i < leftFoot_handleRenderer.Length; i++){
+            leftFoot_handleRenderer[i].enabled = leftFoot_Vis;
+            } 
+            for(int i = 0; i < rightFoot_handleRenderer.Length; i++){
+            rightFoot_handleRenderer[i].enabled = rightFoot_Vis;
+            }         
+            for(int i = 0; i < root_handleRenderer.Length; i++){
+            root_handleRenderer[i].enabled = root_Vis;
+            } 
+        }
+
+
+    // Visibility OFF Button
+        [Button("Visibility OFF", ButtonMode.DisabledInPlayMode, ButtonSpacing.Before)]
+            void VisOff(){
+            head_handleRenderer.enabled = false;
+
+            for(int i = 0; i < leftHand_handleRenderer.Length; i++){
+            leftHand_handleRenderer[i].enabled = false;
+            } 
+            for(int i = 0; i < rightHand_handleRenderer.Length; i++){
+            rightHand_handleRenderer[i].enabled = false;
+            } 
+            for(int i = 0; i < leftFoot_handleRenderer.Length; i++){
+            leftFoot_handleRenderer[i].enabled = false;
+            } 
+            for(int i = 0; i < rightFoot_handleRenderer.Length; i++){
+            rightFoot_handleRenderer[i].enabled = false;
+            }         
+            for(int i = 0; i < root_handleRenderer.Length; i++){
+            root_handleRenderer[i].enabled = false;
+            }            
+
+            Debug.Log("Set Visibility OFF on all Handles");     
+        }
+
+    // Visibility ON Button
+        [Button("Visibility ON", ButtonMode.DisabledInPlayMode, ButtonSpacing.After)]
+        void VisOn(){
+            head_handleRenderer.enabled = true;
+            
+            for(int i = 0; i < leftHand_handleRenderer.Length; i++){
+            leftHand_handleRenderer[i].enabled = true;
+            } 
+            for(int i = 0; i < rightHand_handleRenderer.Length; i++){
+            rightHand_handleRenderer[i].enabled = true;
+            } 
+            for(int i = 0; i < leftFoot_handleRenderer.Length; i++){
+            leftFoot_handleRenderer[i].enabled = true;
+            } 
+            for(int i = 0; i < rightFoot_handleRenderer.Length; i++){
+            rightFoot_handleRenderer[i].enabled = true;
+            }         
+            for(int i = 0; i < root_handleRenderer.Length; i++){
+            root_handleRenderer[i].enabled = true;
+            }    
+
+            Debug.Log("Set Visibility ON on all Handles");
+        }    
+    }
+}
