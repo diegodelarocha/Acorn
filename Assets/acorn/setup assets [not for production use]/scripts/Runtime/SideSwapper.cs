799743050202c54f161cccd0a96eb666f8cd2ae0
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2020-05-29 13:43:42 -0700

    added FK support to side swapper

--- Assets/acorn/setup assets [not for production use]/scripts/Runtime/SideSwapper.cs
+++ Assets/acorn/setup assets [not for production use]/scripts/Runtime/SideSwapper.cs
@@ -11,14 +11,16 @@ namespace Acorn
 {
     public class SideSwapper : MonoBehaviour
     {
-    /// <summary>
-    ///Mirror's poses, useful for stuff like walk cycles.
-    /// </summary>
+        /// <summary>
+        ///Mirror's poses, useful for stuff like walk cycles.
+        /// </summary>
         [SerializeField]
-        [HideInInspector] Transform headIKHandle;
+        [HideInInspector]
+        Transform[] individualSwaps;
 
         [SerializeField]
-        [HideInInspector] IKPair[] IKHandlePairs;
+        [HideInInspector]
+        IKPair[] swapPairs;
 
         Quaternion mirrorRotationNormal = new Quaternion(1, 0, 0, 0);
 
@@ -27,52 +29,92 @@ namespace Acorn
         [Button("Swap Side", ButtonSpacing.Before)]
         void SwapSide()
         {
-            if (undoObjects == null)
-            {
-                undoObjects = IKHandlePairs.SelectMany(pair => new Transform[] { pair.leftIKHandle, pair.rightIKHandle })
-                .Concat(new Transform[] { headIKHandle })
-                .Select(s => s as Object)
-                .ToArray();
-            }
 
-    #if UNITY_EDITOR
+            swapPairs = swapPairs.OrderByDescending(s => GetChildDepth(s.leftIKHandle)).ToArray();
+            individualSwaps = individualSwaps.OrderByDescending(s => GetChildDepth(s)).ToArray();
+
+            undoObjects = swapPairs.SelectMany(pair => new Transform[] { pair.leftIKHandle, pair.rightIKHandle })
+            .Concat(individualSwaps)
+            .Select(s => s as Object)
+            .ToArray();
+
+            Vector3 mirrorNomral = transform.right;
+            //mirrorRotationNormal = new Quaternion(transform.right.x, transform.right.y, transform.right.z, 0);
+
+
+
+#if UNITY_EDITOR
             Undo.RecordObjects(undoObjects, "Swap Side");
-    #endif
-            Vector3 newHeadPosition = headIKHandle.localPosition;
-            newHeadPosition.x = -newHeadPosition.x;
-
-            Quaternion newHeadRotation = headIKHandle.localRotation;
-            newHeadRotation = mirrorRotationNormal * newHeadRotation * mirrorRotationNormal;
-
-            headIKHandle.localPosition = newHeadPosition;
-            headIKHandle.localRotation = newHeadRotation;
+#endif
 
 
 
-            for (int i = 0; i < IKHandlePairs.Length; i++)
+            for (int i = 0; i < swapPairs.Length; i++)
             {
-                Vector3 newRightPosition = IKHandlePairs[i].leftIKHandle.localPosition;
-                newRightPosition.x = IKHandlePairs[i].rightIKHandle.localPosition.x;
-                Quaternion newRightRotation = IKHandlePairs[i].leftIKHandle.localRotation;
-                newRightRotation = mirrorRotationNormal * newRightRotation * mirrorRotationNormal;
+
+                Vector3 newRightPosition = MirrorPosition(swapPairs[i].leftIKHandle.localPosition);
+                Quaternion newRightRotation = MirrorQuaternion(swapPairs[i].leftIKHandle.localRotation);
 
 
-                Vector3 newLeftPosition = IKHandlePairs[i].rightIKHandle.localPosition;
-                newLeftPosition.x = IKHandlePairs[i].leftIKHandle.localPosition.x;
-                Quaternion newLeftRotation = IKHandlePairs[i].rightIKHandle.localRotation;
-                newLeftRotation = mirrorRotationNormal * newLeftRotation * mirrorRotationNormal;
+                Vector3 newLeftPosition = MirrorPosition(swapPairs[i].rightIKHandle.localPosition);
+                Quaternion newLeftRotation = MirrorQuaternion(swapPairs[i].rightIKHandle.localRotation);
 
 
 
 
-                IKHandlePairs[i].leftIKHandle.localPosition = newLeftPosition;
-                IKHandlePairs[i].leftIKHandle.localRotation = newLeftRotation;
+                swapPairs[i].leftIKHandle.localPosition = newLeftPosition;
+                swapPairs[i].leftIKHandle.localRotation = newLeftRotation;
 
-                IKHandlePairs[i].rightIKHandle.localPosition = newRightPosition;
-                IKHandlePairs[i].rightIKHandle.localRotation = newRightRotation;
+                swapPairs[i].rightIKHandle.localPosition = newRightPosition;
+                swapPairs[i].rightIKHandle.localRotation = newRightRotation;
 
-                //IKHandles[i].transform.position = Vector3.Reflect(IKHandles[i].transform.position, transform.right);
             }
+
+            for (int i = 0; i < individualSwaps.Length; i++)
+            {
+
+                Vector3 newIndividualPosition = MirrorPosition(individualSwaps[i].localPosition);
+
+                Quaternion newIndividualRotation = MirrorQuaternion(individualSwaps[i].localRotation);
+
+                individualSwaps[i].localPosition = newIndividualPosition;
+                individualSwaps[i].localRotation = newIndividualRotation;
+            }
+        }
+        Vector3 MirrorPosition(Vector3 position)
+        {
+            Vector3 normal = transform.right;
+            return Vector3.Reflect(position, normal);
+        }
+        Quaternion MirrorQuaternion(Quaternion quaternion)
+        {
+            Vector3 normal = transform.right;
+
+            Quaternion mirrorNormalQuat = new Quaternion(normal.x, normal.y, normal.z, 0).normalized;
+            return mirrorNormalQuat * quaternion * mirrorNormalQuat;
+        }
+
+
+        Quaternion GetRelativeRotation(Quaternion parentRotation, Quaternion childRotation)
+        {
+            return Quaternion.Inverse(parentRotation) * childRotation;
+        }
+
+        static int GetChildDepth(Transform child)
+        {
+            SideSwapper thisScript = child.GetComponentInParent<SideSwapper>();
+            if (!thisScript)
+            {
+                return -1;
+            }
+            int depthIndex = 0;
+            Transform currentParent = child.parent;
+            while (currentParent.transform != thisScript.transform)
+            {
+                depthIndex++;
+                currentParent = currentParent.parent;
+            }
+            return depthIndex;
         }
 
         [System.Serializable]
